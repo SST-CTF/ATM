d16296e14ff8b14a6d181c79494d711eb2861adc
SST-CTF <andryseko@yahoo.com>
2016-06-24 21:30:05 -0700

    Improved everything

--- ATM.py
+++ ATM.py
@@ -7,16 +7,13 @@ pinNumber = input("Please enter your four digit pin: ")
 # TEST IF PIN MATCHES
 
 #Check Balance function here
-def check_balance():
+def check_balance(cardNumber):
     from account.txt import balance
     print(balance)
 
-
-check_balance()
-
 #Deposit function here
-def deposit():
-    global balance
+def deposit(cardNumber):
+    balance = 4
     deposit_value = int(input("How much do you want to deposit? "))
     balance += deposit_value
     print("Your new balance is " + str(balance))
@@ -32,19 +29,17 @@ def withdrawal(cardNumber):
     else:
         print("Error: Not enough balance.")
 
+# Selection is made here, each selection leads to a function above
 selection = 0
 while selection < 4:
     selection = input("What would you like to do today?:\n 1. Check balance\n 2. Deposit Money\n 3. Withdraw Money\n 4. Exit\n\n")
-    if selection is 1:  # The if keyword starts out the if elif else combo. Make sure you have a colon after the condition.
+    if selection is 1: 
         checkBalance(cardNumber)
-    elif selection is 2:  # The is keyword is similar to ==. It tests if something is equal to another thing. elif is optional.
+    elif selection is 2: 
         deposit(cardNumber)
-    elif selection is 3:  # You can have no elif, or many elif statements
+    elif selection is 3: 
         withdrawal(cardNumber)
-    elif selection is 4:  # You can have no elif, or many elif statements
+    elif selection is 4: 
         print("Thank you, and have a nice day!")
         import sys
 sys.exit()
-
-
-
